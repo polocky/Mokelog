cbb337ef7253b43bcc8b4775a30ab3c5e1c449e5
polocky <teranishi@livedoor.jp>
2010-08-20 16:49:03 +0900

    bug fix

--- lib/Mokelog/Data/Project.pm
+++ lib/Mokelog/Data/Project.pm
@@ -16,27 +16,6 @@ __PACKAGE__->setup_alias({
         id => 'project_id',
         });
 
-__PACKAGE__->add_trigger(
-    pre_save => sub {
-        my ( $obj,$orig_obj) = @_;
-        if(my $members = $orig_obj->members()){
-            $members = ','. $members . ',';
-            $obj->members($members);
-            $orig_obj->members($members);
-        }
-    },
-);
-__PACKAGE__->add_trigger(
-    post_load => sub {
-        my ( $obj ) = @_;
-        if(my $members = $obj->members ){
-           $members =~ s/^,//; 
-           $members =~ s/,$//; 
-           $obj->members($members);
-        }
-    }
-);
-
 sub sexy_description {
     my $self = shift;
     my $text = Mokelog::Text->instance();
@@ -48,7 +27,8 @@ sub is_authorized {
     my $user = shift;
     return 1 unless $self->members;
     return 1 if $user eq $self->created_by;
-    return $self->members =~ /\,$user\,/ ? 1 : 0;
+    my $members = ',' .  $self->members . ',';
+    return $members =~ /\,$user\,/ ? 1 : 0;
 }
 
 1;
